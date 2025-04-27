d0b8b830a2dfe3f65fca7b9465df6c48e617a98d
sabrina kassie <sabrinakassie@gmail.com>
2025-04-27 12:52:37 -0400

    Commit 2 - implemented second model on the web

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load model and data
-model = joblib.load("model.pkl")
+model = joblib.load("model1.pkl")
 df = pd.read_csv("cleaned_traffic_violations1.csv")
 
 # Flask app
@@ -36,6 +36,32 @@ def index():
         ].round(3).values.tolist()
 
     return render_template("index.html", results=results)
+
+model1 = joblib.load("logistic_regression_violation_model.pkl")
+
+# First route: latitude + longitude input
+@app.route("/specific_locations", methods=["GET", "POST"])
+def specific_locations():
+    results = None
+    if request.method == "POST":
+        latitude = float(request.form["latitude"])
+        longitude = float(request.form["longitude"])
+
+        user_input = [latitude, longitude]
+
+        # Predict probabilities
+        result = []
+        for v in model1.keys():
+            model = model1[v]
+            p = model.predict_proba([user_input])[0][1]
+            result.append((v, p))
+
+        # Sort and select top 4
+        top_violations = sorted(result, key=lambda x: x[1], reverse=True)[:4]
+
+        results = [(vio, round(prob, 3)) for vio, prob in top_violations]
+
+    return render_template("specific_locations.html", results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
