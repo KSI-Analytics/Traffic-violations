679506e0f9132a239c71908cdf7f0d12b0f4a4b0
sabrina kassie <sabrinakassie@gmail.com>
2025-04-30 14:45:57 -0400

    Add startup.sh

--- Traffic-violations/startup.sh
+++ Traffic-violations/startup.sh
@@ -1,4 +1,11 @@
-gunicorn app:server --bind=0.0.0.0 --timeout 600
+#!/bin/bash
+
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+# Run gunicorn
+gunicorn app:server --workers 4 --bind=0.0.0.0:8000
+
 
 
 
